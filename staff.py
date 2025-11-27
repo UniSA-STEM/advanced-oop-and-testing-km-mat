74923b132ebd559621141dd8dd43dcffa9afb222
Karl Matillano <matky024@mymail.unisa.edu.au>
2025-11-27 17:47:47 +1100

    added basic staff actions

--- staff.py
+++ staff.py
@@ -9,8 +9,17 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 class Staff:
     def __init__(self, name):
-        self.name = name
+        self._name = name
 
     def __str__(self):
         return self.name
 
+class Zookeper(Staff):
+    def feed_animal(self, animal):
+        return f"{self._name} feeds {animal._name}. {animal.eat()}"
+
+    def clean_enclosure(self, enclosure):
+        return f"{self._name} cleans {enclosure._name}. {enclosure.clean()}"
+
+    def health_check(self, animal):
+        return f"{self._name} performs healthcheck on {animal._name}."
