3f5936999eb9c92b177d5b19756c29391c2bfdee
Karl Matillano <matky024@mymail.unisa.edu.au>
2025-11-27 20:11:22 +1100

    added abstract method because why not?

--- animal.py
+++ animal.py
@@ -6,10 +6,9 @@ ID: 110336447
 Username: matky024
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
-from enclosure import Enclosure
 
-
-class Animal:
+from abc import ABC, abstractmethod
+class Animal(ABC):
     def __init__(self, name, species, age, diet):
         self._name = name
         self._age = age
@@ -18,17 +17,17 @@ class Animal:
         self._health_record = []
 
     #animal actions
+    @abstractmethod
     def make_sound(self):
-        return f"{self._species} makes a sound."
-
+        pass
+    @abstractmethod
+    def move(self):
+        pass
     def eat(self):
         return f"{self._species} eats {self._diet}."
 
     def sleep(self):
         return f"{self._species} sleeps."
-
-    def move(self):
-        return f"{self._name} moves."
 
     def info(self):
         return f"{self._name} is a {self._species}, aged {self._age} and eats {self._diet}."
