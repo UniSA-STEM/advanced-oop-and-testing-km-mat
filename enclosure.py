4eb2ac87871fd957988483732a3056f70ce4f8e8
Karl Matillano <matky024@mymail.unisa.edu.au>
2025-11-27 17:39:55 +1100

    added enclosure actions incluiding animal additions

--- enclosure.py
+++ enclosure.py
@@ -12,6 +12,28 @@ class enclosure:
         self._name = name
         self._env_type = env_type
         self._propertysize = size
-        self.cleanliness = 100
+        self._cleanliness = 100
         self._allowed_species = allowed_species
         self._animal = []
+
+    def add_animal(self, animal):
+        if animal._species not in self._allowed_species:
+            print(f"The {animal} is not allowed due to specie limitations   .")
+            self._animal.append(animal)
+
+    def remove_animal(self, animal):
+        if animal in self._animal:
+            self._animal.remove(animal)
+
+    def clean(self):
+        self._cleanliness = 100
+
+    @property
+    def status(self):
+        return{
+            "name:": self._name,
+            "environment": self._env_type,
+            "property size": self._propertysize,
+            "allowed species": self._allowed_species,
+            "animals": [a._name for a in self._animal]
+            "cleanliness": self._cleanliness
